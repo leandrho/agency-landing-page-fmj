1a36ce5c6e08ba73f1627bdfd36277fecdb089d5
Leandro Ruiz <lean@localhost.localdomain>
2024-07-08 15:50:53 -0300

    add header section

---
+++ script.js
@@ -0,0 +1,6 @@
+const sidemenubtn = document.querySelector('.nav-sidemenu-btn');
+sidemenubtn.addEventListener('click',(e)=>{
+    const navbar = document.querySelector('.navbar-nav');
+    navbar.classList.toggle('show');
+    console.log('????')
+});
